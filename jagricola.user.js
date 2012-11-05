d8e2c3c9582ca88b83f7ac39f40572b4309f6efc
Yu MORIYA <euro21st@gmail.com>
2012-11-06 01:05:18 +0900

    add messages.txt

--- jagricola.user.js
+++ jagricola.user.js
@@ -1,11 +1,16 @@
 // ==UserScript==
-// @name           JAgricola
-// @author         euro_s
-// @description    Agricola sites translates to Japanese.
-// @version        0.1
-// @match          http://www.boiteajeux.net/jeux/agr/*
+// @name        JAgricola
+// @namespace   JAgricola
+// @description Agricola sites translates to Japanese.
+// @include     http://www.boiteajeux.net/jeux/agr/*
+// @version     1
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js
+// @grant       hoge
 // ==/UserScript==
 
-(function() {
+(function(){
 	document.body.innerHTML = document.body.innerHTML.replace("Choose an action in the first tab on the left !", "左から選んでね。");
+	$('td[title*="11."]').click(function() {
+	    alert("畑だおっお");
+	});
 })();
