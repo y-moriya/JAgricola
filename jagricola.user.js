b2357f0a95d298ac918293c0b35a5f41180931c3
Yu MORIYA <euro21st@gmail.com>
2012-11-07 01:32:23 +0900

    TODO追記。。

--- jagricola.user.js
+++ jagricola.user.js
@@ -4,13 +4,19 @@
 // @description Agricola sites translates to Japanese.
 // @include     http://www.boiteajeux.net/jeux/agr/*
 // @version     1
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js
+// @require     http://code.jquery.com/jquery-1.8.2.js
 // @grant       hoge
 // ==/UserScript==
 
-(function(){
+// TODO: 手札を表示する箱を用意して日本語で表示。
+// TODO: 各カードクリック？で詳細表示。
+// TODO: メッセージ洗い出して日本語化
+
+(function() {
 	document.body.innerHTML = document.body.innerHTML.replace("Choose an action in the first tab on the left !", "左から選んでね。");
+	$("#dvPanneauInfo").after('<div id="jagmsg" />');
 	$('td[title*="11."]').click(function() {
-	    alert("畑だおっお");
+	    $("#jagmsg").text("畑だおっお");
 	});
+	
 })();
