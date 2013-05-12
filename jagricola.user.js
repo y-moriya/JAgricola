f7b1bd5264c5410775107079cddcf30e373274e4
Yu MORIYA <euro21st@gmail.com>
2013-05-12 09:15:44 +0900

    バージョン更新

--- jagricola.user.js
+++ jagricola.user.js
@@ -3,7 +3,7 @@
 // @namespace   JAgricola
 // @description Agricola sites translates to Japanese.
 // @include     http://www.boiteajeux.net/jeux/agr/partie.php*
-// @version     2.0.1
+// @version     2.0.2
 // @require     http://code.jquery.com/jquery-1.8.2.js
 // @grant       hoge
 // ==/UserScript==
