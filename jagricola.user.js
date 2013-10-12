a6ba571bca17892dedccba5e3f3653105a70a354
swtw7466 <swtw7466@gmail.com>
2013-10-12 15:00:30 +0900

    jQuery を minify 版に変更

--- jagricola.user.js
+++ jagricola.user.js
@@ -4,7 +4,7 @@
 // @description Agricola sites translates to Japanese.
 // @include     http://www.boiteajeux.net/jeux/agr/partie.php*
 // @version     2.0.2
-// @require     http://code.jquery.com/jquery-1.8.2.js
+// @require     http://code.jquery.com/jquery-1.8.2.min.js
 // @require     https://raw.github.com/kswedberg/jquery-cluetip/master/jquery.cluetip.min.js
 // @grant       hoge
 // ==/UserScript==
